78df2f6f3fd2f32c97e2a01d1e3d4fdfbe4d4eb3
jarppe <jarppe@metosin.fi>
2014-11-30 17:32:51 +0200

    gh-pages

--- src/cljs/joulukalenteri/hatch_pos.cljs
+++
@@ -1,9 +0,0 @@
-(ns joulukalenteri.hatch-pos)
-
-(def hatch-positions [
-  {:x   20,  :y   20,  :w  100,  :h  100}
-  {:x  140,  :y   60,  :w  150,  :h   70}
-  {:x  240,  :y  160,  :w  250,  :h  170}
-  {:x  540,  :y  100,  :w   50,  :h  370}
-  {:x  140,  :y  300,  :w   50,  :h  370}
-  ])
--- src/cljs/joulukalenteri/util.cljs
+++
@@ -1,9 +0,0 @@
-(ns joulukalenteri.util)
-
-(defn hatch-count []
-  3
-  #_(let [now (js/Date.)]
-     (cond
-       (> (.getYear now) 114)  24
-       (< (.getMonth now) 11)  0
-       :else (.getDate now))))
